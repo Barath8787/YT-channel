f5941e20bc7cccadb290db5fbfa296f7475343d7
unknown <barathsankar8787@gmail.com>
2025-06-19 12:35:11 +0100

    Test: simplified Home.jsx to debug blank screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
- main.jsx
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 import { HashRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <App />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from 'react'
-import './Home.css'
-import Sidebar from '../../Component/Sidebar/Sidebar'
-import Feed from '../../Component/Feed/Feed'
-function Home({sidebar}) {
-  const[catagory,setcatagory]=useState(0)
+// import React, { useState } from 'react'
+// import './Home.css'
+// import Sidebar from '../../Component/Sidebar/Sidebar'
+// import Feed from '../../Component/Feed/Feed'
+// function Home({sidebar}) {
+//   const[catagory,setcatagory]=useState(0)
+//   return (
+//     <>
+//     <Sidebar sidebar={sidebar} catagory={catagory} setcatagory={setcatagory}/>
+//     <div className={`container ${sidebar?'':'large-container'}`}>
+//       <Feed catagory={catagory}/>
+//     </div>
+//     </>
+//   )
+// }
+
+// export default Home
+import React from 'react';
+
+function Home() {
   return (
-    <>
-    <Sidebar sidebar={sidebar} catagory={catagory} setcatagory={setcatagory}/>
-    <div className={`container ${sidebar?'':'large-container'}`}>
-      <Feed catagory={catagory}/>
+    <div>
+      <h1>Hello, the Home route is working ✅</h1>
     </div>
-    </>
-  )
+  );
 }
 
-export default Home
+export default Home;
